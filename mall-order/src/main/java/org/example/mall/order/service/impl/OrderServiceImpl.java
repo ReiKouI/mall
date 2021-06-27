db5f4bc22f4c7518082da1a4f522f1b0353fc3e3
Eric <156691639@qq.com>
2021-06-27 14:32:06 +0800

    order整合sentinel,需要额外运行sentinel dashboard

--- mall-order/src/main/java/org/example/mall/order/service/impl/OrderServiceImpl.java
+++ mall-order/src/main/java/org/example/mall/order/service/impl/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package org.example.mall.order.service.impl;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import lombok.extern.slf4j.Slf4j;
 import org.example.mall.order.service.OrderService;
 import org.springframework.stereotype.Service;
@@ -9,12 +10,14 @@ import org.springframework.stereotype.Service;
 public class OrderServiceImpl implements OrderService {
 
     @Override
+    @SentinelResource(value = "createOrder")
     public boolean createOrder() {
         log.info("createOrder!");
         return true;
     }
 
     @Override
+    @SentinelResource(value = "updateOrderStatus")
     public boolean updateOrderStatus() {
         log.info("updateOrderStatus!");
         return true;
